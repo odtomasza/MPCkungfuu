0018660df2e83ae57948ac4cebad1a576b23fd22
Tomasz <od.tomasza@gmail.com>
2016-12-14 22:10:12 +0100

    Add files via upload

---
+++ js/index_user_scripts.js
@@ -0,0 +1,66 @@
+
+  function play(){
+       var audio = document.getElementById("audio");
+      audio.play(); 
+      
+                 }
+function play2(){
+       var audio = document.getElementById("audio2");
+      audio.play(); 
+      
+                 }
+function play3(){
+       var audio = document.getElementById("audio3");
+      audio.play(); 
+      
+                 }
+function play4(){
+       var audio = document.getElementById("audio4");
+      audio.play(); 
+      
+                 }
+function play5(){
+       var audio = document.getElementById("audio5");
+      audio.play(); 
+      
+                 }
+function play6(){
+       var audio = document.getElementById("audio6");
+      audio.play(); 
+      
+                 }
+function play7(){
+       var audio = document.getElementById("audio7");
+      audio.play(); 
+      
+                 }
+function play8(){
+       var audio = document.getElementById("audio8");
+      audio.play(); 
+      
+                 }
+function play9(){
+       var audio = document.getElementById("audio9");
+      audio.play(); 
+      
+                 }
+function play10(){
+       var audio = document.getElementById("audio10");
+      audio.play(); 
+      
+                 }
+function play11(){
+       var audio = document.getElementById("audio11");
+      audio.play(); 
+      
+                 }
+function play12(){
+       var audio = document.getElementById("audio12");
+      audio.play(); 
+      
+                 }
+function play13(){
+       var audio = document.getElementById("audio13");
+      audio.play(); 
+      
+                 }
